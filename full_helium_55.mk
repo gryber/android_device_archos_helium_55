4328d9037a5846cc46373d1598cdbbeb8c096c7f
vitek999 <vitek2012rus@gmail.com>
2017-01-09 16:48:18 +0300

    helium: switch to aicp

---
+++ full_helium_55.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+#$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := Helium 55
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/archos/helium_55/device.mk)
+
+# Configure dalvik heap
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := helium_55
+PRODUCT_NAME := aicp_helium_55
+PRODUCT_BRAND := ARCHOS
+PRODUCT_MODEL := Helium 55
+PRODUCT_MANUFACTURER := ARCHOS
